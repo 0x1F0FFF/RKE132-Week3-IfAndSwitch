f34c7d1b8e8e328e19d38c57b639b203f9596bb0
Morten <morten@scripts.ee>
2024-02-07 18:38:07 +0200

    add week 3 code

---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,33 @@
+﻿int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek); // 0 - 6
+//int weekDay = 7; // lihtsalt test
+
+switch (weekDay) // kontrollitav väärtus
+{
+    case 0: // case 0 on samaväärne if (weekDay == 0)
+        Console.WriteLine("It's Sunday!");
+        break;
+    case 1:
+        Console.WriteLine("It's Monday, never lucky...");
+        break;
+    case 2:
+        Console.WriteLine("It's Tuesday.");
+        break;
+    case 3:
+        Console.WriteLine("It's Wednesday.");
+        break;
+    case 4:
+        Console.WriteLine("It's Thusrday");
+        break;
+    case 5:
+        Console.WriteLine("It's Friday.");
+        break;
+    case 6:
+        Console.WriteLine("It's Saturday.");
+        break;
+    default:
+        Console.WriteLine("Oops... Look's like your calendar is broken.");
+        break;
+        
+}
+
+Console.WriteLine("Have a nice day!");
